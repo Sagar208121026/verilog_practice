1bbb107b161353f1c0768d3efdf541bac9db8889
Sagar Verma <verma.sagar826@gmail.com>
2026-06-24 01:33:35 +0530

    Simple 2 ff synchronizer

---
+++ 06_sync/2ff_sync.v
@@ -0,0 +1,33 @@
+module sync_2ff(
+            input clk_dst,
+            input rst_n,
+            input async_in,
+            output sync_out
+
+);
+
+reg sync1,sync2;
+
+always @(posedge clk_dst or negedge rst_n) begin
+    
+    if (~rst_n) begin
+    sync1 <= 0;
+    sync2 <= 0;
+    
+
+    end
+
+    else begin
+
+    sync1 <= async_in;
+    sync2 <= sync1;
+
+    end
+
+    
+end
+
+   assign  sync_out = sync2;
+
+
+endmodule
